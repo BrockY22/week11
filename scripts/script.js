7fc7f5379e0ce726f8035814c0b1c93ba6ddd8bb
Brock <zyuan18@my.bcit.ca>
2023-03-22 13:46:04 -0700

    texxtContent EX

--- scripts/script.js
+++ scripts/script.js
@@ -13,18 +13,29 @@
 
 // greet(input1);
 // greet(input1,"bye","boy","girl");
+
+
+// let radius;
+// do{
+//     radius = prompt("Enter the radius of a circle");
+//     if(isNaN(radius)){
+//         alert("Not a valid radius")
+//     }
+//     
+//     alert(`The area of a circle with radius ${radius} is ${area(radius)}`);
+//     
+// }
+// while(isNaN(radius))
 let area = (radius=0) => {
     return radius * radius * Math.PI;
 }
+radius = prompt("Enter the radius of a circle");
 
-let radius;
-do{
-    radius = prompt("Enter the radius of a circle");
-    if(isNaN(radius)){
-        alert("Not a valid radius")
-    }
-    else{
-        alert(`The area of a circle with radius ${radius} is ${area(radius)}`);
-    }
+const radiuspara = document.querySelector("#radius");
+const resultpara = document.querySelector("#result");
+radiuspara.textContent += radius;
+if (isNaN(radius)) {
+    result.textContent = "ERROR: Invalid radius";
 }
-while(isNaN(radius))
+else
+    result.textContent = `The area of a circle with radius ${radius} is ${area(radius)}`;
