f3f1e26a99dfd743cce02ef1280601b4c975e6be
Brock <zyuan18@my.bcit.ca>
2023-03-22 12:20:50 -0700

    hello arrow function

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-function greet(message){
-    console.log("Hello " + message + "!");
-}
-greet("Neda");
+// function greet(message){
+//     console.log("Hello " + message + "!");
+// }
+// greet("Neda");
+
+let greet = (message) => {console.log("Hello " + message + "!");}
+let input = prompt("Enter your name");
+greet(input);
+greet();
+
