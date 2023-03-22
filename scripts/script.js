5144ecfe4b56b98fa02c513c3d306d71d59f53c5
Brock <zyuan18@my.bcit.ca>
2023-03-22 12:43:43 -0700

    area of circle practice

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,28 @@
 // }
 // greet("Neda");
 
-let greet = (message) => {console.log("Hello " + message + "!");}
-let input = prompt("Enter your name");
-greet(input);
-greet();
+// let greet = (message) => {console.log("Hello " + message + "!");}
+// let input = prompt("Enter your name");
+// greet(input);
+// greet();
 
+// let greet = (message, time="day") => {console.log(`Good ${time} ${message}!`);}
+// let input1 = prompt("Enter your name");
+
+// greet(input1);
+// greet(input1,"bye","boy","girl");
+let area = (radius=0) => {
+    return radius * radius * Math.PI;
+}
+
+let radius;
+do{
+    radius = prompt("Enter the radius of a circle");
+    if(isNaN(radius)){
+        alert("Not a valid radius")
+    }
+    else{
+        alert(`The area of a circle with radius ${radius} is ${area(radius)}`);
+    }
+}
+while(isNaN(radius))
